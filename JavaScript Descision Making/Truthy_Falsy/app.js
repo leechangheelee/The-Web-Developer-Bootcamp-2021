fe4d814035358f5eb221fdeec382853dcd87e82d
이창희 <9919star@gmail.com>
2021-10-15 14:42:11 +0900

    Truth-y & False-y Values 연습

---
+++ JavaScript Descision Making/Truthy_Falsy/app.js
@@ -0,0 +1,32 @@
+const userInput = prompt("Enter something");
+
+if (userInput) {
+    console.log("TRUTHY!");
+} else {
+    console.log("FALSY!");
+}
+
+
+// if (0) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+// if (NaN) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+// if (undefined) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+if (' ') {
+    console.log("TRUTHY");
+} else {
+    console.log("FALSY");
+}

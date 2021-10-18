9ce767fc66317f91de300a13f8b63b15f40f0589
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-10-18 22:20:44 +0900

    Switch Statement 연습

---
+++ JavaScript Descision Making/Switch/app.js
@@ -0,0 +1,43 @@
+const day = 723;
+switch(day) {
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    case 4:
+        console.log("THURSDAY");
+        break;
+    case 5:
+        console.log("FRIDAY");
+        break;
+    case 6:
+    case 7:
+        console.log("WEEKEND!");
+        break;
+    default:
+        console.log("I DONT KNOW THAT!")
+}
+
+// if(day === 1) {
+//     console.log("MONDAY");
+// }
+// else if(day === 2) {
+//     console.log("TUESDAY");
+// }
+// else if(day === 3) {
+//     console.log("WEDNESDAY");
+// }
+// else if(day === 4) {
+//     console.log("THURSDAY");
+// }
+// else if(day === 5) {
+//     console.log("FRIDAY");
+// }
+// else {
+//     console.log("I DONT KNOW THAT!");
+// }

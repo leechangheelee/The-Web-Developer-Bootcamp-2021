7511e3702637e97aed7bdca0fbfeb2fb4ea7375c
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-10-18 22:03:25 +0900

    Logical NOT 연습

--- JavaScript Descision Making/Logical_Operators/app.js
+++ JavaScript Descision Making/Logical_Operators/app.js
@@ -5,13 +5,23 @@
 //     console.log("INCORRECT FORMAT FOR PASSWORD!");
 // }
 
-const age = 50;
-if((age >= 0 && age < 5) || age >= 65){
-    console.log("FREE");
-} else if (age >= 5 && age < 10) {
-    console.log("$10");
-} else if (age >= 10 && age < 65) {
-    console.log("$20");
-} else {
-    console.log("INVALID AGE!");
+// const age = 50;
+// if((age >= 0 && age < 5) || age >= 65){
+//     console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20");
+// } else {
+//     console.log("INVALID AGE!");
+// }
+
+// let firstName = prompt("enter your first name");
+// if (!firstName) {
+//     firstName = prompt("TRY AGAIN!!!");
+// }
+
+const age = 100;
+if(!((age >= 0 && age < 5) || age >= 65)){
+    console.log("YOU ARE NOT A BABY OR A SENIOR!");
 }

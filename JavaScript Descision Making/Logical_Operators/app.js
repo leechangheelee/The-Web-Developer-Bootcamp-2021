a7bf5709a56bf7abbbdcec1f6b6c59dc231357e1
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-10-18 21:08:35 +0900

    Logical OR 연습

--- JavaScript Descision Making/Logical_Operators/app.js
+++ JavaScript Descision Making/Logical_Operators/app.js
@@ -1,6 +1,17 @@
-const password = prompt("Enter your password");
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-    console.log("VALID PASSWORD!");
+// const password = prompt("Enter your password");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//     console.log("VALID PASSWORD!");
+// } else {
+//     console.log("INCORRECT FORMAT FOR PASSWORD!");
+// }
+
+const age = 50;
+if((age >= 0 && age < 5) || age >= 65){
+    console.log("FREE");
+} else if (age >= 5 && age < 10) {
+    console.log("$10");
+} else if (age >= 10 && age < 65) {
+    console.log("$20");
 } else {
-    console.log("INCORRECT FORMAT FOR PASSWORD!");
+    console.log("INVALID AGE!");
 }

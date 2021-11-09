306874cc877e174d9090d04a3efa37a215fba75d
이창희 <9919star@gmail.com>
2021-11-09 16:36:11 +0900

    Looping Over Arrays 연습

--- JavaScript/Repeating Stuff With Loops/ForLoops/app.js
+++ JavaScript/Repeating Stuff With Loops/ForLoops/app.js
@@ -11,6 +11,26 @@
 //     console.log(i);
 // }
 
-for(let i = 10; i <= 1000; i *= 10) {
-    console.log(i);
+// for(let i = 10; i <= 1000; i *= 10) {
+//     console.log(i);
+// }
+
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i);
+// }
+
+const animals = [
+    "lions",
+    "tigers",
+    "bears"
+];
+
+// for(let i=0; i<animals.length; i++){
+//     console.log(i, animals[i]);
+// }
+
+for(let i=animals.length-1; i>=0; i--){
+    console.log(i, animals[i]);
 }

c1d55e2b80921829c93f3e57f3e9bb9743b69833
이창희 <9919star@gmail.com>
2021-11-09 14:40:16 +0900

    More For Loops Practice

--- JavaScript/Repeating Stuff With Loops/ForLoops/app.js
+++ JavaScript/Repeating Stuff With Loops/ForLoops/app.js
@@ -1,4 +1,16 @@
-for(let num = 1; num<=10; num+=1){
-    console.log("IM IN THE LOOP BODY!!!");
-    console.log(num);
+// for(let num = 1; num<=10; num+=1){
+//     console.log("IM IN THE LOOP BODY!!!");
+//     console.log(num);
+// }
+
+// for(let i = 1; i <= 20; i+=2) {
+//     console.log(i);
+// }
+
+// for(let i = 100; i >= 0; i-=10) {
+//     console.log(i);
+// }
+
+for(let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
 }

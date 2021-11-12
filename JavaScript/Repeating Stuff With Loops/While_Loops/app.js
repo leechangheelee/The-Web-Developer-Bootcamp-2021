aa5c6aa6610a735f09702a9e27087082a0c1dfb5
이창희 <9919star@gmail.com>
2021-11-12 12:14:35 +0900

    The Break Keyword 연습

--- JavaScript/Repeating Stuff With Loops/While_Loops/app.js
+++ JavaScript/Repeating Stuff With Loops/While_Loops/app.js
@@ -4,10 +4,24 @@
 //     console.log(count);
 // }
 
-const SECRET = "BabyHippo";
+// const SECRET = "BabyHippo";
 
-let guess = prompt("enter the secret code...");
-while(guess !== SECRET) {
-    guess = prompt("enter the secret code...");
+// let guess = prompt("enter the secret code...");
+// while(guess !== SECRET) {
+//     guess = prompt("enter the secret code...");
+// }
+// console.log("CONGRATS YOU GOT THE SECRET!!!");
+
+// let input = prompt("Hey, say something!");
+// while(true) {
+//     input = prompt(input);
+//     if(input.toLowerCase() === "stop copying me") {
+//         break;
+//     }
+// }
+// console.log("OK YOU WIN!");
+
+for(let i = 0; i < 1000; i++){
+    console.log(i);
+    if(i === 100) break;
 }
-console.log("CONGRATS YOU GOT THE SECRET!!!");

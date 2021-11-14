1ededf1beccfdbd81d5d8a6c1dfed22a123c28f6
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-11-14 18:03:18 +0900

    For...Of Loop 연습

---
+++ JavaScript/Repeating Stuff With Loops/ForOf/app.js
@@ -0,0 +1,27 @@
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+// for(let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
+
+// for(let subreddit of subreddits) {
+//     console.log(`Visit reddit.com/r/${subreddit}`)
+// }
+
+
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for(let row of seatingChart) {
+//     for(let student of row) {
+//         console.log(student);
+//     }
+// }
+
+// for(let char of "hello world") {
+//     console.log(char);
+// }
+

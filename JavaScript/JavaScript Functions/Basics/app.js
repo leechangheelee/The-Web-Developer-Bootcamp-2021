39956a3beaeebc54003417d9e0a6dbbab617b06e
이창희 <9919star@gmail.com>
2021-11-17 16:03:30 +0900

    Functions With Multiple Arguments 연습

--- JavaScript/JavaScript Functions/Basics/app.js
+++ JavaScript/JavaScript Functions/Basics/app.js
@@ -16,6 +16,15 @@ function singSong() {
 // singSong()
 // singSong()
 
-function greet(firstName) {
-    console.log(`Hey there, ${firstName}!`);
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`);
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for(let i=0; i<numTimes; i++) {
+        result += str;
+    }
+
+    console.log(result);
 }

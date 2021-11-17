39e31a2e5649f38a2bee6fb2f830306f05f237a7
이창희 <9919star@gmail.com>
2021-11-17 15:11:01 +0900

    Argument Intro 연습

--- JavaScript/JavaScript Functions/Basics/app.js
+++ JavaScript/JavaScript Functions/Basics/app.js
@@ -1,17 +1,21 @@
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-
 function singSong() {
     console.log("DO");
     console.log("RE");
     console.log("MI");
+}
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
+function greet(firstName) {
+    console.log(`Hey there, ${firstName}!`);
 }

249dd4c471da459cf2d2bd7e8e97485d7d299543
이창희 <9919star@gmail.com>
2021-11-17 14:57:15 +0900

    Our Very First Function 연습

---
+++ JavaScript/JavaScript Functions/Basics/app.js
@@ -0,0 +1,17 @@
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}

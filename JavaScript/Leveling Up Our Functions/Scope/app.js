6d104b509133dcb4bba6c19f51be4d87f48c026e
이창희 <9919star@gmail.com>
2021-11-18 12:58:43 +0900

    Function Scope 연습

---
+++ JavaScript/Leveling Up Our Functions/Scope/app.js
@@ -0,0 +1,15 @@
+// let totalEggs = 0;
+// function collectEggs(){
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+let bird = 'Scarlet Macaw';
+function birdWatch() {
+    let bird = 'Great Blue Heron';
+    console.log(bird);
+}
+
+birdWatch();

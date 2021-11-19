79bec4ddc7812b1f21a57248099f33659b0811bc
이창희 <9919star@gmail.com>
2021-11-19 11:00:12 +0900

    Block Scope 연습

--- JavaScript/Leveling Up Our Functions/Scope/app.js
+++ JavaScript/Leveling Up Our Functions/Scope/app.js
@@ -6,10 +6,26 @@
 // collectEggs();
 // console.log(totalEggs);
 
-let bird = 'Scarlet Macaw';
-function birdWatch() {
-    let bird = 'Great Blue Heron';
-    console.log(bird);
-}
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
 
-birdWatch();
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!';
+// }
+
+// console.log(radius);
+// console.log(msg);
+
+for(let i = 0; i < 5; i++) {
+    let msg = "ASKLDJAKLSJD";
+    console.log(msg);
+}
+console.log(msg);
+console.log(i);

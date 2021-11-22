565f69741c42206f25f37f3fa151eb05408b6067
이창희 <9919star@gmail.com>
2021-11-22 17:21:35 +0900

    Arrow Functions Wrapup 연습

--- JavaScript/Callbacks & Array Methods/ArrowFunctions/app.js
+++ JavaScript/Callbacks & Array Methods/ArrowFunctions/app.js
@@ -4,24 +4,35 @@
 //     return num * 2;
 // })
 
-// const movies = [
-//     {
-//         title: 'Amadeus',
-//         score: 99
-//     },
-//     {
-//         title: 'Stand By Me',
-//         score: 85
-//     },
-//     {
-//         title: 'Parasite',
-//         score: 95
-//     },
-//     {
-//         title: 'Alien',
-//         score: 90
-//     }
-// ]
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+
+// const newMovies = movies.map(function(movie){
+//     return `${movie.title} - ${movie.score/10}`
+// })
+
+// const newMovies = movies.map(movie => `${movie.title} - ${movie.score/10}`)
+
+const newMovies = movies.map(movie => (
+    `${movie.title} - ${movie.score/10}`
+))
 
 // const titles = movies.map(function(movie){
 //     return movie.title.toUpperCase();

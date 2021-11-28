f44ca112207dc86e91c159be5c3fd8ef21771083
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-11-28 16:16:55 +0900

    Default Params 연습

---
+++ JavaScript/Newer JavaScript Features/DefaultParams/app.js
@@ -0,0 +1,13 @@
+// function rollDie(numSides) {
+//     if(numSides === undefined) numSides = 6;
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
+
+function rollDie(numSides = 6) {
+    return Math.floor(Math.random() * numSides) + 1
+}
+
+function greet(person, msg="Hey there", punc = '!') {
+    console.log(`${msg}, ${person}${punc}`);
+}

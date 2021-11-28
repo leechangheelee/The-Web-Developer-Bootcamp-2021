bea74fc94ac979dae7db3c3af3dafe04395868b3
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-11-28 17:10:47 +0900

    Spread with Objects 연습

--- JavaScript/Newer JavaScript Features/Spread/app.js
+++ JavaScript/Newer JavaScript Features/Spread/app.js
@@ -7,3 +7,19 @@ const cats = ['Blue', 'Scout', 'Rocket'];
 const dogs = ['Rusty', 'Wyatt'];
 
 const allPets = [...cats, ...dogs];
+
+
+
+const feline = {legs: 4, family: 'Felidae'};
+const canine = {isFurry: true, family: 'Caninae'};
+
+const catDog = {...feline, ...canine};
+
+
+const dataFromForm = {
+    email: 'blueman@gmail.com',
+    password: 'tobias123!',
+    username: 'tfunke'
+}
+
+const newUser = {...dataFromForm, id: 2345, isAdmin: false};

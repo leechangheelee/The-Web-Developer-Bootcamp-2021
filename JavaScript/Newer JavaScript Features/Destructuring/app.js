8903d72af6ebe04121efff0403c8954f63ad7443
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-11-30 22:31:24 +0900

    Destructuring Objects 연습

--- JavaScript/Newer JavaScript Features/Destructuring/app.js
+++ JavaScript/Newer JavaScript Features/Destructuring/app.js
@@ -1,6 +1,37 @@
-const scores = [929321, 899341, 888336, 772739, 543671, 243567, 111934];
+// const scores = [929321, 899341, 888336, 772739, 543671, 243567, 111934];
 
-const highScore = scores[0];
-const secondHighScore = scores[1];
+// const highScore = scores[0];
+// const secondHighScore = scores[1];
 
-const [gold, silver, bronze, ...everyoneElse] = scores
+// const [gold, silver, bronze, ...everyoneElse] = scores
+
+const user = {
+    email: 'harvey@gmail.com',
+    password: 'sCoTt1948sMiTh',
+    firstName: 'Harvey',
+    lastName: 'Milk',
+    born: 1930,
+    died: 1978,
+    bio: 'Harvey Bernad Milk was an American politician and the first openly ...',
+    city: 'San Francisco',
+    state: 'California'
+}
+
+const user2 = {
+    email: 'Stacy@gmail.com',
+    firstName: 'Stacy',
+    lastName: 'Gonzalez',
+    born: 1987,
+    city: 'Tulsa',
+    state: 'Oklahoma'
+}
+
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+// const email = user.email;
+
+// const { email, firstName, lastName, city, bio } = user;
+
+const { born: birthYear, died: deathYear = 'N/A' } = user;
+
+// const { city, state, died = 'N/A'} = user2;

67c2eacc43e445267f242cd12848438c4c243be5
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-11-30 22:11:44 +0900

    Destructuring Arrays 연습

---
+++ JavaScript/Newer JavaScript Features/Destructuring/app.js
@@ -0,0 +1,6 @@
+const scores = [929321, 899341, 888336, 772739, 543671, 243567, 111934];
+
+const highScore = scores[0];
+const secondHighScore = scores[1];
+
+const [gold, silver, bronze, ...everyoneElse] = scores

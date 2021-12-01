781d7ad95ad347187246f0d58011b5bd374412fe
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-12-01 23:02:32 +0900

    querySelector & querySelectorAll 연습

--- JavaScript/DOM/Selecting/app.js
+++ JavaScript/DOM/Selecting/app.js
@@ -5,8 +5,14 @@
 //     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
 // }
 
-const squareImages = document.getElementsByClassName('square');
+// const squareImages = document.getElementsByClassName('square');
 
-for(let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// for(let img of squareImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+const links = document.querySelectorAll('p a');
+
+for(let link of links) {
+    console.log(link.href)
 }

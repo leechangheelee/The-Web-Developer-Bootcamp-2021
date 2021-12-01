4938732df15b6a23e5fc07956dcadea9988ad367
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-12-01 22:49:20 +0900

    getElementsByTagName & className 연습

---
+++ JavaScript/DOM/Selecting/app.js
@@ -0,0 +1,12 @@
+// const allImages = document.getElementsByTagName('img')
+
+// for(let image of allImages){
+//     //console.log(image.src);
+//     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+const squareImages = document.getElementsByClassName('square');
+
+for(let img of squareImages) {
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+}

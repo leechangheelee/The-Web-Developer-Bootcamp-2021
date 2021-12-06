ab10640a9df02f81b221dc801a12d380b374b27a
이창희 <9919star@gmail.com>
2021-12-06 15:43:30 +0900

    addEventListner 연습

--- JavaScript/DOM Event/EventBasics/app.js
+++ JavaScript/DOM Event/EventBasics/app.js
@@ -15,3 +15,29 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clicked the h1');
 }
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', () => {
+    alert("CLICKED!");
+});
+
+function twist() {
+    console.log("TWIST!");
+}
+
+function shout() {
+    console.log("SHOUT!");
+}
+
+const tasButton = document.querySelector('#tas');
+
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+// 아래 코드는 한번씩만 실행됨
+// tasButton.addEventListener('click', twist, { once: true });
+// tasButton.addEventListener('click', shout, { once: true });
+
+tasButton.addEventListener('click', twist);
+tasButton.addEventListener('click', shout);

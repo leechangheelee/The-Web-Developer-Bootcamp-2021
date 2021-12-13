0fad6f59e23ccd2e5660770157771a8aa4f154ce
이창희 <9919star@gmail.com>
2021-12-13 12:53:24 +0900

    Event Bubbling 연습

---
+++ JavaScript/DOM Event/BubblingAndDelegation/app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('#changeColor');
+const container = document.querySelector('#container');
+
+button.addEventListener('click', function(e) {
+    container.style.backgroundColor = makeRandColor();
+    e.stopPropagation();
+});
+
+container.addEventListener('click', function () {
+    container.classList.toggle('hide');
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}

6b2c5f31434b02291af4c08eedfccafe8c7e0080
이창희 <9919star@gmail.com>
2021-12-16 15:56:17 +0900

    Score Keeper Pt. 3 With Bulma 연습

--- JavaScript/Score Keeper/app.js
+++ JavaScript/Score Keeper/app.js
@@ -16,8 +16,10 @@ p1Button.addEventListener('click', function () {
         if(p1Score === winningScore) {
             isGameOver = true;
 
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -29,8 +31,10 @@ p2Button.addEventListener('click', function () {
         if(p2Score === winningScore) {
             isGameOver = true;
 
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -49,6 +53,8 @@ function reset () {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }

bc31ecb02342ed9923aac45a63d9b4b7f970cfc6
이창희 <9919star@gmail.com>
2021-12-16 14:55:04 +0900

    Score Keeper Pt. 2 연습

--- JavaScript/Score Keeper/app.js
+++ JavaScript/Score Keeper/app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -13,6 +15,9 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if(p1Score === winningScore) {
             isGameOver = true;
+
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
     }
@@ -23,7 +28,27 @@ p2Button.addEventListener('click', function () {
         p2Score += 1;
         if(p2Score === winningScore) {
             isGameOver = true;
+
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
         p2Display.textContent = p2Score;
     }
 })
+
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset () {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}

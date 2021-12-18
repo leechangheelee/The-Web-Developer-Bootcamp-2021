c9da5dcb4f93cec781769d0df8e102adbb6ce32d
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-12-19 00:52:31 +0900

    Callback Hell 연습

---
+++ JavaScript/Async JavaScript/CallbackHell/app.js
@@ -0,0 +1,46 @@
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext && doNext();
+    }, delay)
+}
+
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('green', 1000, () => {
+                delayedColorChange('blue', 1000, () => {
+        
+                })
+            })
+        })
+    })
+});
+
+// 콜백 설계 예시
+searchMoviesAPI('madeus', () => {
+    saveToMyDB(movies, () => {
+        //if it works, run this:
+    }, () => {
+        //if it doesn't work, run this:
+    })
+}, () => {
+    //if API is down, or request failed
+})

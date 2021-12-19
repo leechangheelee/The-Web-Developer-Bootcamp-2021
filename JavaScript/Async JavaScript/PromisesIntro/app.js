8a6e1834baed5ba943593e54fbb9292e500dc527
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-12-19 14:57:02 +0900

    Demo: fakeRequest Using Promises 연습

--- JavaScript/Async JavaScript/PromisesIntro/app.js
+++ JavaScript/Async JavaScript/PromisesIntro/app.js
@@ -23,57 +23,51 @@ const fakeRequestPromise = (url) => {
     })
 }
 
-fakeRequestCallback('books.com/page1',
-    function (response) {
-        console.log("IT WORKED!");
-        console.log(response);
-        fakeRequestCallback('books.com/page2',
-            function (response) {
-                console.log("IT WORKED AGAIN!!!");
-                console.log(response);
-                fakeRequestCallback('bookgs.com/page3',
-                    function (response) {
-                        console.log("IT WORKED AGAIN (3rd req)!!!");
-                        console.log(response);
-                    },
-                    function (err) {
-                        console.log("ERROR (3rd req)!!!", err);
-                    })
-            },
-            function (err) {
-                console.log("ERROR (2nd req)!!!", err);        
-            })
-    }, function (err) {
-        console.log("ERROR!!!", err);
-    });
-
 // fakeRequestCallback('books.com/page1',
 //     function (response) {
-//         console.log("IT WORKED!!!!")
-//         console.log(response)
+//         console.log("IT WORKED!");
+//         console.log(response);
 //         fakeRequestCallback('books.com/page2',
 //             function (response) {
-//                 console.log("IT WORKED AGAIN!!!!")
-//                 console.log(response)
-//                 fakeRequestCallback('books.com/page3',
+//                 console.log("IT WORKED AGAIN!!!");
+//                 console.log(response);
+//                 fakeRequestCallback('bookgs.com/page3',
 //                     function (response) {
-//                         console.log("IT WORKED AGAIN (3rd req)!!!!")
-//                         console.log(response)
+//                         console.log("IT WORKED AGAIN (3rd req)!!!");
+//                         console.log(response);
 //                     },
 //                     function (err) {
-//                         console.log("ERROR (3rd req)!!!", err)
+//                         console.log("ERROR (3rd req)!!!", err);
 //                     })
 //             },
 //             function (err) {
-//                 console.log("ERROR (2nd req)!!!", err)
+//                 console.log("ERROR (2nd req)!!!", err);        
 //             })
 //     }, function (err) {
-//         console.log("ERROR!!!", err)
-//     })
+//         console.log("ERROR!!!", err);
+//     });
 
-
-
-
+fakeRequestPromise('yelp.com/api/coffee/page1')
+    .then(() => {
+        console.log("IT WORKED!!! (page1)")
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+            .then(() => {
+                console.log("IT WORKED!!! (page2)")
+                fakeRequestPromise('yelp.com/api/coffee/page3')
+                    .then(() => {
+                        console.log("IT WORKED!!! (page3)")
+                    })
+                    .catch(() => {
+                        console.log("OH NO, ERROR!! (page3)")
+                    })
+            })
+            .catch(() => {
+                console.log("OH NO, ERROR!! (page2)")
+            })
+    })
+    .catch(() => {
+        console.log("OH NO, ERROR!! (page1)")
+    })
 
 
 

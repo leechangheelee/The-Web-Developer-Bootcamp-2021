b511492a5d8fb60ad8b4c492b66ce604c68aea64
leechangheelee <79301439+leechangheelee@users.noreply.github.com>
2021-12-21 23:15:11 +0900

    Setting Headers With Axios 연습

--- JavaScript/AJAX and API's/Axios/app.js
+++ JavaScript/AJAX and API's/Axios/app.js
@@ -15,3 +15,27 @@ const fetchBitcoinPrice = async () => {
         console.log("ERROR!", e);
     }
 }
+
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    //console.log(jokeText);
+    const newLI = document.createElement('li');
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+
+const getDadJoke = async() => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        //console.log(res.data.joke);
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
+    }
+}
+
+button.addEventListener('click', addNewJoke);
